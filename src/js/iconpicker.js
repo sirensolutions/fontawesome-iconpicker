899d0a9a8cd9197d2691425b9d21beae42814cc0
Felixls <sergioscarnatto@gmail.com>
2017-04-28 14:47:34 +0100

    Event mouseup don't return after hide the iconpicker UI

--- src/js/iconpicker.js
+++ src/js/iconpicker.js
@@ -348,6 +348,7 @@
                     $doc.on('mouseup' + _eventNs, function(e) {
                         if (!_self._isEventInsideIconpicker(e) && !_self.isInline()) {
                             _self.hide();
+                            return;
                         }
                         e.stopPropagation();
                         e.preventDefault();
